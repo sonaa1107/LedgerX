f97d173956333ef3be7f98f3e77b43ec8ec127cb
sonaa1107 <sonaagrawal1107@gmail.com>
2026-03-30 23:42:45 +0530

    small changes

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose')
+
+function connectToDb(){
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("database connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+module.exports=connectToDb
